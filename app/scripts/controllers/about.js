d11973b7aaeb75f7240796a9f29a9b62bfae1243
stephanie.hauck <stephanie.hauck@oracle.com>
2017-02-11 14:34:20 -0700

    initial commit - yoeman angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name learningResourceWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the learningResourceWebAppApp
+ */
+angular.module('learningResourceWebAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name learningResourceWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the learningResourceWebAppApp
+ */
+angular.module('learningResourceWebAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
